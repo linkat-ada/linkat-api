c74114ebe00c502365c30e89b251ceeb12815167
ali almanea <ali.1995almanea@gmail.com>
2022-06-22 13:50:25 +0300

    firebase&qrcodesss

--- api/lib/firebase/services/index.js
+++ api/lib/firebase/services/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const app = require("../config");
 const { getStorage } = require("firebase/storage");
 const storage = getStorage(app);
@@ -37,43 +36,3 @@ const uplaod = async (file, res, req) => {
 module.exports = {
   uplaod,
 };
-=======
-const app = require("../config");
-const { getStorage } = require("firebase/storage");
-const storage = getStorage(app);
-const { ref, uploadBytes, getDownloadURL } = require("firebase/storage");
-const response = require("../../helper/responses");
-
-const uplaod = async (file, res, req) => {
-  try {
-    if (req?.file) {
-      console.log(file)
-      const uniqueFileName = `images/${new Date().valueOf()}.png`;
-      const imageRef = ref(storage, uniqueFileName);
-      const metaType = {
-        contentType: file?.mimetype,
-        name: file?.originalname,
-      };
-      await uploadBytes(imageRef, file?.buffer, metaType)
-        .then(async () => {
-          let publicUrl = await getDownloadURL(imageRef);
-          return publicUrl
-        })
-        .catch((e) => {
-          console.error(e);
-          return response.failedWithMessage(e.message, res);
-        });
-    }
-    return null;
-  } catch (err) {
-    console.log("ERROR---->", err);
-    throw new Error();
-  }
-};
-
-
-
-module.exports = {
-  uplaod,
-};
->>>>>>> 46e39038d741ef44ff7d52b4529a7e1bdb6e74dd
