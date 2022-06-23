5b8ed29263752bf3f49fdf3eec5291a5894f6273
ABDRABO ALDHVADI <abdrabo@ABDRABOs-MacBook-Pro.local>
2022-06-23 14:04:01 +0300

    firebaseand restruct repo

--- client/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import taskReducer from "./taskReducer";
-
-const allReducers = combineReducers({
-    task: taskReducer
-})
-
-export default allReducers
--- client/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from "redux";
-
-import allReducers from "./reducers/index";
-
-const store = createStore(
-    allReducers
-)
-
-export default store
