03957b29150c140b24e63433e64695c5a7b3d8ec
zaabta <ali.1995almanea@gmail.com>
2022-07-18 16:54:12 +0300

    fixing deploy init

--- routes/apiRoutesV1.js
+++ routes/apiRoutesV1.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.use("/users", require("../lib/users/routes"));
 router.use("/links", require("../lib/links/routes"));
-router.use("/qrcodes", require("../lib/qrcodes/routes"));
+router.use("/qrcodes", require("../lib/qrCodes/routes"));
 router.use("/admins", require("../lib/admins/routes"));
 router.use("/superadmins", require("../lib/superAdmins/routes"));
 router.use("/subscribers", require("../lib/subscribers/routers"));
