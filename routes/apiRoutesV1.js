afe9838eabe0bc8cdc3f44278f1d5cb6dfd5b933
abdulrahmanalhelali <ayash2063@hotmail.com>
2022-07-19 17:52:55 +0400

    deleted useless route

--- routes/apiRoutesV1.js
+++ routes/apiRoutesV1.js
@@ -2,10 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 
-router.use("/", (req, res) => {
-    let welcomeHTML = `<h1>Welcome to the todo api v1</h1> <a target="_blank"  href="https://documenter.getpostman.com/view/20338203/UzQxM45n">Click here for the API Documentation</a>`;
-    res.send(welcomeHTML);
-});
 router.use("/users", require("../lib/users/routes"));
 router.use("/links", require("../lib/links/routes"));
 router.use("/qrcodes", require("../lib/qrCodes/routes"));
